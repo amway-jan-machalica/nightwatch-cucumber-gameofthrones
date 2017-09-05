1c565f66141c2cb932d9e285a3fdbcee3b7bed58
Jan Machalica <jan.machalica@bold.net.pl>
2017-09-05 14:48:30 +0200

    cleanup CC

--- index.js
+++ index.js
@@ -3,16 +3,16 @@ var app = express();
 var hbs = require('express-hbs');
 
 app.engine('hbs', hbs.express4({
-  partialsDir: __dirname + '/views/partials'
+    partialsDir: __dirname + '/views/partials'
 }));
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 
 
 app.get('/testexpress', function (req, res) {
-  res.send('hello world');
+    res.send('hello world');
 });
 
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+    console.log('Example app listening on port 3000!');
 });
