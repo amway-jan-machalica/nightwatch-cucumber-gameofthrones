a0cbe9e24378515ff6e85393c3d4078937443357
Jan Machalica <jan.machalica@bold.net.pl>
2017-09-05 09:59:03 +0200

    implementing vesteros world

--- index.js
+++ index.js
@@ -10,16 +10,34 @@ app.engine('hbs', hbs.express4({
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 
+app.use('/assets', express.static('assets'));
+
+/**
+ * Sword image: http://www.i2clipart.com/cliparts/e/a/2/1/clipart-sword-ea21.png
+ */
 app.get('/vesteros', (req, res) => {
-  res.render('vesteros');
+  res.render('vesteros',
+    {
+      families: [
+        'Lannister',
+        'Baratheon',
+        'Stark',
+        'Tyrell'
+      ], 
+      pageTitle: 'Vesteros'
+    });
 });
 
 app.get('/essos', (req, res) => {
-  res.render('essos');
+  res.render('essos', {
+    pageTitle: 'Essos'
+  });
 });
 
 app.get('/', (req, res) => {
-  res.render('map');
+  res.render('map', {
+    pageTitle: 'Map'
+  });
 })
 
 app.get('/testexpress', function (req, res) {
@@ -28,4 +46,8 @@ app.get('/testexpress', function (req, res) {
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
+});
+
+hbs.registerHelper('lowerCase', function(str) {
+  return str.toLowerCase();
 });
